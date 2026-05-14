d45787d483b9f05e914633eedcdeb63408839c8d
Alejandroski33-bin <alecafer99@gmail.com>
2026-05-14 14:22:34 -0600

    Privacy adjustments

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
+const apiKey = import.meta.env.VITE_API_KEY;
 export const supabaseUrl = "https://qmpoddsnorcdjzdowyvz.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFtcG9kZHNub3JjZGp6ZG93eXZ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU4NzAxMTksImV4cCI6MjA5MTQ0NjExOX0.5AJe4Qw9RxbfGhDpu7yfQgYhB6zngEfZGJRE21vCpns";
+const supabaseKey = apiKey;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
